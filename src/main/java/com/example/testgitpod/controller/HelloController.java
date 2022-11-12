49833c42ca4563e27f3ba635a46602732b579153
jcdominguez <jcd.pro@gmail.com>
2022-11-12 10:28:23 +0100

    add JPA + MySQL

--- src/main/java/com/example/testgitpod/controller/HelloController.java
+++ src/main/java/com/example/testgitpod/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.testgitpod;
+package com.example.testgitpod.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
